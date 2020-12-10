b1a1845a1ec821f2d1fa8c4e2179435b4c8100fc
BjoBor <67904970+BjoBor@users.noreply.github.com>
2020-12-10 14:47:08 +0100

    .

---
+++ BluetoothControlApp/PiBot/obj/Debug/90/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,10 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "PiBot.dll" comes first in this list. */
+		"PiBot.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+	public static String ApiPackageName = "Mono.Android.Platform.ApiLevel_28";
+}
